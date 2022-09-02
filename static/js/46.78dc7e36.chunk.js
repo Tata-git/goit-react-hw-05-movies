dce3b6cec62ee1adfd6122fd852057bedf962dfa
Tata-git <83451272+Tata-git@users.noreply.github.com>
2022-09-02 15:40:07 +0000

    Deploying to gh-pages from @ Tata-git/goit-react-hw-05-movies@f345abfd58cd44e184b29e3031097baaf54b5034 🚀

---
+++ static/js/46.78dc7e36.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[46],{9046:function(e,t,n){n.r(t);var o=n(501),r=n(184);t.default=function(){return(0,r.jsxs)("h1",{children:["Content not found. Go to ",(0,r.jsx)(o.rU,{to:"/",children:"home page."})]})}}}]);
+//# sourceMappingURL=46.78dc7e36.chunk.js.map
